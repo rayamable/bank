7f32729113cbba8dae09f0eaecf5243b59c89ed8
Ray Amable <rayamable@gmail.com>
2019-08-15 21:49:47 +0000

    Added deposit and withdrawal

--- app/assets/javascripts/bank_accounts/Show.js
+++ app/assets/javascripts/bank_accounts/Show.js
@@ -1,22 +1,48 @@
 var Show = (function() {
   var $btnNewTransaction;
-  var $modalTransaction;
   var $btnSave;
   var $inputAmount;
+  var $modalTransaction;
   var $selectTransactionType;
+  var $parameters; 
+
+  var bankAccountId;
 
   var fetchElements = function() {
     $btnNewTransaction     = $("#btn-new-transaction");
-    $modalTransaction      = $("#modal-transaction");
     $btnSave               = $("#btn-save");
     $inputAmount           = $("#input-amount");
+    $modalTransaction      = $("#modal-transaction");    
     $selectTransactionType = $("#select-transaction-type");
+    $parameters            = $("#parameters");
+
+    bankAccountId          = $parameters.data("bank-account-id");
   };
 
-  var initializeEvents =function() {
+  var disableControls = function() {
+    $btnSave.prop("disabled", true);
+    $inputAmount.prop("disabled", true);
+    $selectTransactionType.prop("disabled", true);
+  }; 
+
+  var enableControls = function() {
+    $btnSave.prop("disabled", false);
+    $inputAmount.prop("disabled", false);
+    $selectTransactionType.prop("disabled", false);
+  }; 
+
+  var initializeEvents = function() {
     $btnNewTransaction.on("click", function() {
       $modalTransaction.modal("show");
-   });       
+   });
+
+    $btnSave.on("click", function() {
+      var amount          = $inputAmount.val();
+      var transactionType = $selectTransactionType.val();
+      disableControls();
+
+      console.log("Amount: " + amount + "Transaction Type: " + transactionType + " Bank Account ID: " + bankAccountId);        
+    });       
  };
 
  var init = function() {
