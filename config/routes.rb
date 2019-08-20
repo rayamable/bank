ca0c0dfd6e2423420e1b4acbc44776cfae862c3f
Ray Amable <rayamable@gmail.com>
2019-08-20 07:05:20 +0000

    Modified client controller and added new files

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     root to: "pages#index"
     
     resources :bank_accounts
-
-    resources :clients
+    
+    resources :clients do 
+        resources :bank_accounts, controller: "clients/bank_accounts", except: [:index, :show, :destroy] 
+    end
 
     namespace :api do 
         namespace :v1 do 
